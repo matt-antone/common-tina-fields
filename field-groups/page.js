4023631ee7f60d39fcd7bf24fc8e2b773e918578
Matt Antone <matt.antone@gmail.com>
2022-05-24 11:56:25 -0700

    updated required

--- field-groups/page.js
+++ field-groups/page.js
@@ -3,6 +3,7 @@ const page = [
     type: "datetime",
     label: "Posted Date",
     name: "date",
+    required: true,
     ui: {
       dateFormat: "MMMM DD YYYY",
       timeFormat: "hh:mm A",
@@ -12,18 +13,21 @@ const page = [
     type: "string",
     label: "Title",
     name: "title",
+    required: true,
     description: "Title of this page."
   },
   {
     type: "image",
     name: "featuredImg",
     label: "Featured Image",
+    required: false,
     description: "this is something"
   },
   {
     type: "rich-text",
     label: "Excerpt",
     name: "excerpt",
+    required: true,
   },
 ]
 export default page
