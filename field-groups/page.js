30d6a74a4e0ca5551f6228fbfcb7862fa7867ce0
Matt Antone <matt.antone@gmail.com>
2022-05-24 11:27:48 -0700

    added title description

--- field-groups/page.js
+++ field-groups/page.js
@@ -12,85 +12,18 @@ const page = [
     type: "string",
     label: "Title",
     name: "title",
+    description: "Title of this page."
   },
   {
     type: "image",
-    name: "heroImg",
-    label: "Hero Image",
+    name: "featuredImg",
+    label: "Featured Image",
+    description: "this is something"
   },
   {
     type: "rich-text",
     label: "Excerpt",
     name: "excerpt",
-  },
-  {
-    type: "rich-text",
-    label: "Body",
-    name: "_body",
-    templates: [
-      {
-        name: "DateTime",
-        label: "Date & Time",
-        inline: true,
-        fields: [
-          {
-            name: "format",
-            label: "Format",
-            type: "string",
-            options: ["utc", "iso", "local"],
-          },
-        ],
-      },
-      {
-        name: "BlockQuote",
-        label: "Block Quote",
-        fields: [
-          {
-            name: "children",
-            label: "Quote",
-            type: "rich-text",
-          },
-          {
-            name: "authorName",
-            label: "Author",
-            type: "string",
-          },
-        ],
-      },
-      {
-        name: "NewsletterSignup",
-        label: "Newsletter Sign Up",
-        fields: [
-          {
-            name: "children",
-            label: "CTA",
-            type: "rich-text",
-          },
-          {
-            name: "placeholder",
-            label: "Placeholder",
-            type: "string",
-          },
-          {
-            name: "buttonText",
-            label: "Button Text",
-            type: "string",
-          },
-          {
-            name: "disclaimer",
-            label: "Disclaimer",
-            type: "rich-text",
-          },
-        ],
-        ui: {
-          defaultItem: {
-            placeholder: "Enter your email",
-            buttonText: "Notify Me",
-          },
-        },
-      },
-    ],
-    isBody: true,
   },
 ]
 export default page
