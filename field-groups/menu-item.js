1fbf54d769ef24b7bef5f52e7b3b190e539ce61d
Matt Antone <matt.antone@gmail.com>
2022-05-24 12:57:46 -0700

    added menuItems

---
+++ field-groups/menu-item.js
@@ -0,0 +1,49 @@
+export const menuLink = {
+  label: "Menu Item",
+  name: "menuItem",
+  fields: [
+    {
+      name: "label",
+      label: "Label",
+      type: "string",
+      required: true,
+      description: "Provide menu text."
+    },
+    {
+      name: "href",
+      label: "Link",
+      type: "string",
+      required: true,
+      description: "Provide menu url. This can be relative or absolute. Absolute urls will open a new window."
+    },
+  ]
+}
+
+export const subMenuFields = [
+  {
+    name: "hasMenu",
+    label: "Has Menu",
+    type: "boolean",
+    component: "toggle",
+  },
+  {
+    mame: "menuItems",
+    label: "Children",
+    component: "group-list",
+    fields: [
+      ...menuLink.fields,
+    ]
+  }
+]
+
+export const menuItems = {
+  name: 'menuItems',
+  label: "Menu Items",
+  component: "group-list",
+  fields: [
+    ...menuLink,
+    ...subMenuFields,
+  ]
+}
+
+export default menuItems
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import page from './field-groups/page'
+import menuItems from './field-groups/menu-item'
 
 export default {
   page: page,
+  menuItems: menuItems,
 }
